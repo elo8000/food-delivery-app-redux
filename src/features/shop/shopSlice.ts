30867e1a55d4f0d612867b7380d26300c85bac21
elo8000 <elo8000@protonmail.com>
2023-05-31 09:13:02 +0300

    added active shop id to a shop slice

--- src/features/shop/shopSlice.ts
+++ src/features/shop/shopSlice.ts
@@ -15,10 +15,12 @@ export type Shop = {
 
 export interface ShopState {
   shops: Shop[]
+  activeShopId: number
 }
 
 const initialState: ShopState = {
   shops: [],
+  activeShopId: 0,
 }
 
 export const shopSlice = createSlice({
@@ -28,14 +30,18 @@ export const shopSlice = createSlice({
     addShop: (state, action: PayloadAction<Shop>) => {
       state.shops.push(action.payload)
     },
+    setActiveShop: (state, action: PayloadAction<number>) => {
+      state.activeShopId = action.payload
+    },
   },
 })
 
-export const { addShop } = shopSlice.actions
+export const { addShop, setActiveShop } = shopSlice.actions
 
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
 // in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
 export const selectShops = (state: RootState) => state.shop.shops
+export const selectActiveShopId = (state: RootState) => state.shop.activeShopId
 
 export default shopSlice.reducer
