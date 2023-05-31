9718ce7307fdc9c069baa20789559c9312817179
elo8000 <elo8000@protonmail.com>
2023-05-31 16:42:31 +0300

    dockerized + moved migration to a docker service

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,10 @@
 const knex = require("knex")({
   client: "mysql2",
   connection: {
-    host: "127.0.0.1",
+    host: "host.docker.internal",
     port: 3306,
     user: "root",
-    password: "",
+    password: "password",
     database: "test",
   },
   log: {
