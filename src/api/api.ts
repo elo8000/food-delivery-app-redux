3bdc89803d29bb90ad05833d1b21742595683b0f
elo8000 <elo8000@protonmail.com>
2023-05-31 09:12:02 +0300

    added backend api

---
+++ src/api/api.ts
@@ -0,0 +1,32 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+import { ShopItem } from "../features/shop/shopSlice"
+import { CartState } from "../features/cart/cartSlice"
+
+// Define a service using a base URL and expected endpoints
+export const api = createApi({
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000" }),
+  endpoints: (builder) => ({
+    getShops: builder.query<{ id: number; name: string }[], void>({
+      query: () => `/shopNames`,
+    }),
+    getShopItemsById: builder.query<ShopItem[], number>({
+      query: (id) => `/shop/${id}/items`,
+    }),
+    checkout: builder.mutation<string, CartState>({
+      query: (cart: CartState) => ({
+        url: "/checkout",
+        method: "POST",
+        body: cart,
+      }),
+    }),
+  }),
+})
+
+// Export hooks for usage in functional components, which are
+// auto-generated based on the defined endpoints
+export const {
+  useGetShopsQuery,
+  useGetShopItemsByIdQuery,
+  useCheckoutMutation,
+} = api
+export default api
