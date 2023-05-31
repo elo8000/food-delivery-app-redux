ad16f5404c1797bb2d64f73b95fc6ec961ae03f1
elo8000 <elo8000@protonmail.com>
2023-05-31 08:53:00 +0300

    added products

---
+++ src/components/Product/index.tsx
@@ -0,0 +1,21 @@
+import { ShopItem } from "../../features/shop/shopSlice"
+import { addItem } from "../../features/cart/cartSlice"
+import { useDispatch } from "react-redux"
+
+export default function Product(props: { item: ShopItem }) {
+  const dispatch = useDispatch()
+  return (
+    <div className="flex flex-col w-80 h-60 gap-2 border-gray-600 border-2 p-4 rounded-md">
+      <img src={props.item.imageUrl}></img>
+      {props.item.name}
+      <button
+        className="self-end border-2 border-gray-600 bg-gray-200 py-2 px-4 rounded-md"
+        onClick={() => {
+          dispatch(addItem(props.item.id))
+        }}
+      >
+        Add to cart
+      </button>
+    </div>
+  )
+}
---
+++ src/components/Products/index.tsx
@@ -0,0 +1,15 @@
+import { ShopItem } from "../../features/shop/shopSlice"
+import Product from "../Product"
+import styles from "./styels.module.css"
+
+export default function Products(props: { products?: ShopItem[] }) {
+  return (
+    <div
+      className={`grid w-full border-gray-600 border-2 rounded-md p-4 content-baseline gap-4 overflow-y-auto ${styles.products}`}
+    >
+      {props.products?.map((item) => {
+        return <Product item={item} key={item.id} />
+      })}
+    </div>
+  )
+}
