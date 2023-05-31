66334eaeaa3601cc49d2c20367c158b3ed4e42fd
elo8000 <elo8000@protonmail.com>
2023-05-31 08:34:11 +0300

    added main route

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,51 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import { Provider } from "react-redux"
 import { store } from "./app/store"
-import App from "./App"
 import "./index.css"
+import "./components/Header"
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Outlet,
+  Route,
+  RouterProvider,
+} from "react-router-dom"
+import Header from "./components/Header"
+import Shops from "./routes/shops"
+import Cart from "./routes/cart"
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      path="/"
+      element={
+        <div className="flex flex-col h-full">
+          <Header
+            pages={[
+              {
+                name: "Shops",
+                url: "/",
+              },
+              {
+                name: "Cart",
+                url: "/cart",
+              },
+            ]}
+          />
+          <Outlet />
+        </div>
+      }
+    >
+      <Route path="/" element={<Shops />}></Route>
+      <Route path="/cart" element={<Cart items={[]} />}></Route>
+    </Route>,
+  ),
+)
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>,
 )
