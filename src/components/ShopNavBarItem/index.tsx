6d97b1a4af5d5e94d10ee2d441f1b6eb43276c48
elo8000 <elo8000@protonmail.com>
2023-05-31 09:12:18 +0300

    updated shop navbar to use redux logic

--- src/components/ShopNavBarItem/index.tsx
+++ src/components/ShopNavBarItem/index.tsx
@@ -1,14 +1,19 @@
+import { setActiveShop } from "../../features/shop/shopSlice"
+import { useDispatch, useSelector } from "react-redux"
+import { selectActiveShopId } from "../../features/shop/shopSlice"
 import styles from "./styles.module.css"
-export default function ShopNavBarItem(props: {
-  name: string
-  active?: boolean
-  setActive: () => void
-}) {
-  const activeStyle = props.active ? styles.active : ""
+import { selectCartHasItems } from "../../features/cart/cartSlice"
+export default function ShopNavBarItem(props: { name: string; id: number }) {
+  const dispatch = useDispatch()
+  const activeShopId = useSelector(selectActiveShopId)
+  const cartHasItems = useSelector(selectCartHasItems)
+  const activeStyle = props.id === activeShopId ? styles.active : ""
   return (
     <button
       className={`${activeStyle} w-1/2 p-4 rounded-md border-gray-600 border-2`}
-      onClick={props.setActive}
+      onClick={() => {
+        if (!cartHasItems) dispatch(setActiveShop(props.id))
+      }}
     >
       {props.name}
     </button>
