5c1e3313d3a15bdfde5c62fc7d60c14fd0efa2bd
elo8000 <elo8000@protonmail.com>
2023-06-01 16:32:59 +0300

    added error handling

--- backend/src/index.js
+++ backend/src/index.js
@@ -40,45 +40,55 @@ app.get("/shops", async (req, res) => {
   res.send(await knex("shops").select("id", "name", "lat", "lng"));
 });
 app.get("/shop/:id/items", async (req, res) => {
-  res.send(
-    await knex
-      .from("shop_items")
-      .select("id", "name", "price")
-      .where("shop_id", req.params.id)
-  );
-});
-app.post("/checkout", async (req, res) => {
-  const timestamp = new Date().toISOString().slice(0, 19).replace("T", " ");
-  const order = {
-    timestamp: timestamp,
-    user_id: req.body.user.id,
-    address: req.body.user.address,
-    email: req.body.user.email,
-    phone: req.body.user.phone,
-    name: req.body.user.name,
-  };
-  const orderId = (await knex.insert(order, ["id"]).into("orders"))[0].id;
-  const orderItems = req.body.cart.items.map((item) => {
-    return {
-      count: item.count,
-      shop_item_id: item.id,
-      order_id: orderId,
-      price: item.price,
-    };
-  });
-  for (const item of orderItems) {
-    const serverPrice = (
+  try {
+    res.send(
       await knex
         .from("shop_items")
-        .select("price")
-        .where("id", item.shop_item_id)
-    )[0].price;
-    if (serverPrice !== item.price) {
-      throw "User attempting to buy an item listed for a different price than on a server";
-    }
+        .select("id", "name", "price")
+        .where("shop_id", req.params.id)
+    );
+  } catch (e) {
+    res.status(400).send(e);
   }
-  await knex.insert(orderItems).into("order_items");
-  res.status(200).send();
+});
+app.post("/checkout", async (req, res) => {
+  try {
+    knex.transaction(async function (trx) {
+      const timestamp = new Date().toISOString().slice(0, 19).replace("T", " ");
+      const order = {
+        timestamp: timestamp,
+        user_id: req.body.user.id,
+        address: req.body.user.address,
+        email: req.body.user.email,
+        phone: req.body.user.phone,
+        name: req.body.user.name,
+      };
+      const orderId = (await knex.insert(order, ["id"]).into("orders"))[0].id;
+      const orderItems = req.body.cart.items.map((item) => {
+        return {
+          count: item.count,
+          shop_item_id: item.id,
+          order_id: orderId,
+          price: item.price,
+        };
+      });
+      for (const item of orderItems) {
+        const serverPrice = (
+          await knex
+            .from("shop_items")
+            .select("price")
+            .where("id", item.shop_item_id)
+        )[0].price;
+        if (serverPrice !== item.price) {
+          throw "User attempting to buy an item listed for a different price than on a server";
+        }
+      }
+      await knex.insert(orderItems).into("order_items");
+      res.status(200).send();
+    });
+  } catch (e) {
+    res.statusCode(400).send(e);
+  }
 });
 
 app.listen(process.env.PORT || 3000, () => {
