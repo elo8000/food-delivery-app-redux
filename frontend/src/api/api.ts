a30c4adabc9f0d951f0be64e7eb9220b6d2d2d55
elo8000 <elo8000@protonmail.com>
2023-06-01 06:51:30 +0300

    vercel

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -5,7 +5,7 @@ import { CartState } from "../features/cart/cartSlice"
 // Define a service using a base URL and expected endpoints
 export const api = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: process.env.REACT_APP_BACKEND_HOST || "http://localhost:3000",
+    baseUrl: import.meta.env.VITE_BACKEND_HOST || "http://localhost:3000",
   }),
   endpoints: (builder) => ({
     getShops: builder.query<{ id: number; name: string }[], void>({
