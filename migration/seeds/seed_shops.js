18539c9bfa60f0715be105521a8daca0f7415633
elo8000 <elo8000@protonmail.com>
2023-06-01 07:39:39 +0300

    added geolocation to shop

--- migration/seeds/seed_shops.js
+++ migration/seeds/seed_shops.js
@@ -4,6 +4,10 @@ const makeASeedArray = require("../utls/seed/makeASeedArray");
  * @returns { Promise<void> }
  */
 const { faker } = require("@faker-js/faker");
+const KHARKIV_GEOLOCATION = {
+  lat: 49.98081,
+  lng: 36.25272,
+};
 exports.seed = async function (knex) {
   // Deletes ALL existing entries
   await knex("shops").del();
@@ -12,6 +16,16 @@ exports.seed = async function (knex) {
       {
         name: faker.company.buzzNoun,
         address: () => faker.location.streetAddress({ fullName: true }),
+        lat: () =>
+          faker.location.latitude({
+            min: KHARKIV_GEOLOCATION.lat - 2,
+            max: KHARKIV_GEOLOCATION.lat + 2,
+          }),
+        lng: () =>
+          faker.location.latitude({
+            min: KHARKIV_GEOLOCATION.lng - 2,
+            max: KHARKIV_GEOLOCATION.lng + 2,
+          }),
       },
       50
     )
