6050e9d492cf299352418218f1b2662bf5c68530
elo8000 <elo8000@protonmail.com>
2023-06-01 14:12:13 +0300

    css adjustments

--- frontend/src/components/CartItem/index.tsx
+++ frontend/src/components/CartItem/index.tsx
@@ -8,7 +8,9 @@ export default function CartItem(props: { item: CartItemType }) {
   const dispatch = useDispatch()
   return (
     <div className="flex w-full h-60 gap-2 border-gray-600 border-2 p-4 rounded-md">
-      <img src={props.item.imageUrl || "via.placeholder.com/200x100"}></img>
+      <img
+        src={props.item.imageUrl || "http://via.placeholder.com/200x200"}
+      ></img>
       <div>
         <p>{props.item.name}</p>
         <p>Price: {props.item.price} each</p>
@@ -16,13 +18,14 @@ export default function CartItem(props: { item: CartItemType }) {
           className="border-2 border-gray-600"
           type="number"
           onChange={(e) => {
-            dispatch(
-              setItemCount({
-                id: props.item.id,
-                count: Number(e.target.value),
-                price: props.item.price,
-              }),
-            )
+            if (e.target.value.match(/^\d*$/g))
+              dispatch(
+                setItemCount({
+                  id: props.item.id,
+                  count: Number(e.target.value),
+                  price: props.item.price,
+                }),
+              )
           }}
           value={props.item.count}
         />
--- frontend/src/components/Product/index.tsx
+++ frontend/src/components/Product/index.tsx
@@ -6,16 +6,20 @@ export default function Product(props: { item: ShopItem }) {
   const dispatch = useDispatch()
   return (
     <div className="flex flex-col w-80 h-60 gap-2 border-gray-600 border-2 p-4 rounded-md">
-      <img src={props.item.imageUrl}></img>
-      {props.item.name}
-      <button
-        className="self-end border-2 border-gray-600 bg-gray-200 py-2 px-4 rounded-md"
-        onClick={() => {
-          dispatch(addItem({ id: props.item.id, price: props.item.price }))
-        }}
-      >
-        Add to cart
-      </button>
+      <img
+        src={props.item.imageUrl || "http://via.placeholder.com/200x100"}
+      ></img>
+      <div className="flex flex-col h-full justify-end">
+        {props.item.name}
+        <button
+          className="self-end border-2 border-gray-600 bg-gray-200 py-2 px-4 rounded-md"
+          onClick={() => {
+            dispatch(addItem({ id: props.item.id, price: props.item.price }))
+          }}
+        >
+          Add to cart
+        </button>
+      </div>
     </div>
   )
 }
