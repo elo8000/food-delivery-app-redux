2437184e9c29d5cd5dc54006bc8eb1a567f99e2e
elo8000 <elo8000@protonmail.com>
2023-06-01 06:06:31 +0300

    changed backend port

--- backend/src/index.js
+++ backend/src/index.js
@@ -65,8 +65,6 @@ app.post("/checkout", async (req, res) => {
   res.status(200).send();
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`Example app listening on port 3000`);
 });
-
-module.exports = app;
